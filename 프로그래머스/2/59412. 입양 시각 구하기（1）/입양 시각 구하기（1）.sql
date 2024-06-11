b260598a15c74aca3640405b3d8c43806a24f0a7
saious35 <dyghks35@naver.com>
2024-06-11 16:50:28 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -0,0 +1,11 @@
+-- 코드를 입력하세요
+-- 0611
+-- 조건 1 09:00부터 19:59까지, 각 시간대별로 입양이 몇 건이나 발생했는지 조회 
+-- 조건 2 시간대 순으로 정렬
+-- 
+SELECT HOUR(DATETIME) AS HOUR,	COUNT(ANIMAL_ID) AS COUNT
+FROM ANIMAL_OUTS
+GROUP BY HOUR(DATETIME)
+HAVING HOUR >=9 AND HOUR <=19
+ORDER  BY  HOUR(DATETIME)
+
